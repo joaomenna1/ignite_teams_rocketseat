7ab82dfbe6d3790e65fdab1de82144f079631dbd
joaomenna <joaomenna1@gmail.com>
2025-04-08 16:52:01 -0400

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Logo, BackButton, BackIcon } from "./styles";
+import logoImg from "@assets/logo.png";
+
+type props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: props) {
+  return (
+    <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+      <Logo source={logoImg} />
+    </Container>
+  );
+}
